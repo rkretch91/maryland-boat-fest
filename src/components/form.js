269a3be10dc13cf4312d2d0692df25322f0fe99a
Ryan Kretch <rkretch91@gmail.com>
2018-11-22 17:12:14 +0100

    ok

--- src/components/form.js
+++ src/components/form.js
@@ -4,7 +4,7 @@ const Form = () => (
   <div className="field">
     <form className="control">
       <input className="input" type="text" placeholder="Email" />
-      <input className="button" type="submit" placeholder="Submit" />
+      <input className="button" type="submit" placeholder="Submit" style={{float: "right"}} />
     </form>
   </div>
 )
--- src/components/image.js
+++ src/components/image.js
@@ -17,7 +17,7 @@ const Image = () => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "boat-4-02.png" }) {
+        placeholderImage: file(relativePath: { eq: "boat-5-02.png" }) {
           childImageSharp {
             fluid(maxWidth: 600, quality: 100) {
               ...GatsbyImageSharpFluid
