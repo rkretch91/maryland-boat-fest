b82e850f8e88e9ecdd00bb1e52eb3590a0e772b3
Ryan Kretch <rkretch91@gmail.com>
2018-12-01 14:00:50 +0100

    hi

--- src/components/form.js
+++ src/components/form.js
@@ -5,7 +5,7 @@ const Form = () => (
     <form className="control" name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
       <input type="hidden" name="bot-field"/>
       <input className="input" name="email" type="text" id="email" placeholder="Put in a valid email and press enter"/>
-      <input className="button" type="submit" placeholder="Submit" style={{float: "right"}} />
+      <input className="button" type="submit" value="Submit" style={{float: "right"}} />
     </form>
   </div>
 )
