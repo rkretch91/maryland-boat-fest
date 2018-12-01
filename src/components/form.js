e8dc73f97c1bc6b59274227b67e844d849b21581
Ryan Kretch <rkretch91@gmail.com>
2018-12-01 13:44:08 +0100

    hi

--- src/components/form.js
+++ src/components/form.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Form = () => (
   <div className="field">
-    <form className="control" name="contact" method="post" data-netlify="true">
-      <input className="input" type="text" placeholder="Email"/>
+    <form className="control" name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+      <input type="hidden" name="bot-field"/>
+      <input className="input" name="email" type="text" id="email" placeholder="Email"/>
       <input className="button" type="submit" placeholder="Submit" style={{float: "right"}} />
     </form>
   </div>
